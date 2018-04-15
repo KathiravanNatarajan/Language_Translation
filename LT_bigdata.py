325f202f41be2225584a3f3a62a543c817e6496f
KathiravanNatarajan <sunkathirav@gmail.com>
2018-04-15 01:51:51 -0500

    added dataPrep

--- LT_bigdata.py
+++ LT_bigdata.py
@@ -14,8 +14,8 @@ def source_text_to_int(source_filename):
                 else:   
                     source_vocab_to_int[word] = counter
                     counter += 1
-    pickle.dump(source_vocab_to_int, open( "source_vocab_to_int.pkl", "wb" ) )
-    source_vocab_to_int = pickle.load( open( "source_vocab_to_int.pkl", "rb" ) )
+    pickle.dump(source_vocab_to_int, open( "./data/Chinese/source_vocab_to_int.pkl", "wb" ) )
+    source_vocab_to_int = pickle.load( open( "./data/Chinese/source_vocab_to_int.pkl", "rb" ) )
     source_int_text = []
     with open(source_filename, encoding='utf-8') as infile2:
         for line2 in infile2:
@@ -24,8 +24,8 @@ def source_text_to_int(source_filename):
             for word in line2.split():
                 new_list.append(source_vocab_to_int[word])
             source_int_text.append(new_list)
-    pickle.dump((source_int_text,source_vocab_to_int), open( "source_int_txt.pkl", "wb" ) )
-    source_int_text, source_vocab_to_int = pickle.load(open("source_int_txt.pkl", "rb"))
+    pickle.dump((source_int_text,source_vocab_to_int), open( "./data/Chinese/source_int_txt.pkl", "wb" ) )
+    source_int_text, source_vocab_to_int = pickle.load(open("./data/Chinese/source_int_txt.pkl", "rb"))
     return source_int_text, source_vocab_to_int
 
 def target_text_to_int(target_filename):
@@ -41,17 +41,17 @@ def target_text_to_int(target_filename):
                 else:   
                     target_vocab_to_int[word] = counter
                     counter += 1
-    pickle.dump(target_vocab_to_int, open( "target_vocab_to_int.pkl", "wb" ) )
+    pickle.dump(target_vocab_to_int, open( "./data/Chinese/target_vocab_to_int.pkl", "wb" ) )
     target_int_text =[]
-    target_vocab_to_int = pickle.load( open( "target_vocab_to_int.pkl", "rb" ) )
+    target_vocab_to_int = pickle.load( open( "./data/Chinese/target_vocab_to_int.pkl", "rb" ) )
     with open(target_filename, encoding='utf-8') as infile2:
         for line2 in infile2:
             line2 = line2.lower()
             new_list = [] 
             for word2 in line2.split():
-                new_list.append(source_vocab_to_int[word])
+                new_list.append(target_vocab_to_int[word])
             new_list[-1] = target_vocab_to_int['<EOS>']
             target_int_text.append(new_list)
-    pickle.dump((target_int_text,target_vocab_to_int), open( "target_int_txt.pkl", "wb" ) )
-    target_int_text, target_vocab_to_int = pickle.load(open("target_int_txt.pkl", "rb"))
+    pickle.dump((target_int_text,target_vocab_to_int), open( "./data/Chinese/target_int_txt.pkl", "wb" ) )
+    target_int_text, target_vocab_to_int = pickle.load(open("./data/Chinese/target_int_txt.pkl", "rb"))
     return target_int_text, target_vocab_to_int
